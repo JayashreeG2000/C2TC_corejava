94459155ce19406fef4931347937046dc406bc21
nice <nice@DESKTOP-D1AGTR2>
2022-03-29 23:00:55 +0530

    core java

---
+++ Solution/src/primary/Solution.java
@@ -0,0 +1,19 @@
+package primary;
+import java.util.Scanner;
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner in =new Scanner(System.in);
+		System.out.print("Input first number:");
+		int num1= in.nextInt();
+		System.out.print("Input second number:");
+		int num2= in.nextInt();
+		
+		
+	if (num1==num2);
+	   System.out.print("They are same");
+		 
+	}
+	
+
+}
